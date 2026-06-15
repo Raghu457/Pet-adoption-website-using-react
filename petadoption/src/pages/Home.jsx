78ad21c9a70a1867f11421d771627bc2aef04189
S Raghavendra <suradaraghavendra@gmail.com>
2026-06-15 17:35:50 +0530

    firstcommit

--- petadoption/src/pages/Home.jsx
+++ petadoption/src/pages/Home.jsx
@@ -1,22 +1,120 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
 function Home() {
+
+  const [petOfDay, setPetOfDay] =
+    useState(null);
+
+  useEffect(() => {
+
+    async function getPetOfDay() {
+
+      const response =
+        await api.get("/pets");
+
+      const pets =
+        response.data;
+
+      const randomPet =
+        pets[
+          Math.floor(
+            Math.random() *
+            pets.length
+          )
+        ];
+
+      setPetOfDay(
+        randomPet
+      );
+
+    }
+
+    getPetOfDay();
+
+  }, []);
+
   return (
-    <div
-      style={{
-        textAlign: "center",
-        padding: "80px 20px"
-      }}
-    >
-      <h1>🐾 PetPals</h1>
 
-      <p>
-        Find your perfect furry friend.
-      </p>
+    
 
-      <h2>
-        Adopt • Love • Care
-      </h2>
+    <div className="hero">
+
+      <div className="hero-content">
+
+        <h1>
+          🐾PetPals
+        </h1>
+
+        <p>
+          Find your perfect furry friend.
+        </p>
+
+        {
+
+          petOfDay && (
+
+            <div
+              className="pet-day-card"
+            >
+
+              <h2>
+                🏆 Pet Of The Day
+              </h2>
+
+              <img
+                src={
+                  petOfDay.image
+                }
+                alt={
+                  petOfDay.name
+                }
+              />
+
+              <h3>
+                {petOfDay.name}
+              </h3>
+
+              <p>
+                Breed:
+                {" "}
+                {petOfDay.breed}
+              </p>
+
+              <p>
+                Age:
+                {" "}
+                {petOfDay.age}
+              </p>
+
+              <p>
+                Gender:
+                {" "}
+                {petOfDay.gender}
+              </p>
+
+              <p>
+                Adoption Fee:
+                ₹{petOfDay.fee}
+              </p>
+
+              <p>
+                ⭐
+                {petOfDay.rating}
+              </p>
+
+            </div>
+
+          )
+
+        }
+
+      </div>
+
     </div>
+
   );
+
 }
 
 export default Home;
